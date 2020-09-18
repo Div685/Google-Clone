6fdad3499ff3992ed62ada9400dcc5efc55c24b6
Div685 <divyeshpatel685@gmail.com>
2020-09-18 23:29:03 +0300

    add search input and buttons

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../search/Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 function Search() {
+  const [input, setInput] = useState("");
+  const history = useHistory();
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log("you hit a button!!", input);
+
+    history.push("/search");
+  };
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
-
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon />
       </div>
-    </div>
+
+      <div className="search__buttons">
+        <Button type="submit" onClick={search} variant="outlined">
+          Google Search
+        </Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
+      </div>
+    </form>
   );
 }
 
