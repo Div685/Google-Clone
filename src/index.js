ea33aadb2672b33126d6bcd02b21629144abf2ce
Div685 <divyeshpatel685@gmail.com>
2021-11-18 15:33:55 +0300

    update index.js file
    - imported reducer and initialState form reducer file to index.js file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { StateProvider } from './StateProvider';
+import reducer, { initialState } from './reducer';
 
 ReactDOM.render(
   <React.StrictMode>
