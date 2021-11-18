e3b964013fffe30688545d27bf88c3516448177a
Div685 <divyeshpatel685@gmail.com>
2021-11-18 15:30:24 +0300

    Add reducer to reducer.js file
    - Add switch statement to define different case

--- src/reducer.js
+++ src/reducer.js
@@ -6,4 +6,18 @@ export const actionTypes = {
   SET_SEARCH_TERM: "SET_SEARCH_TERM",
 };
 
+const reducer = (state, action) => {
+  console.log(action);
 
+  switch (action.type) {
+    case actionTypes.SET_SEARCH_TERM:
+      return {
+        ...state,
+        term: action.term
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
