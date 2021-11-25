156ef3d9437fb94619449e9973bef6c501a19c9b
Div685 <divyeshpatel685@gmail.com>
2021-11-25 20:17:03 +0300

    Add google apis to fetchData function
    - inside useEffect hook

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -8,7 +8,19 @@ const useGoogleSearch = (term) => {
 
   useEffect(() => {
 
+    const fetchData = async() => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      ).then(response => response.json)
+      .then(result => {
+        setData(result)
+      })
+    }
+
+    fetchData();
   }, [term])
+
+  return { data }
 };
 
 export default useGoogleSearch;
