8ea3decc8190331d8dc1a47f215bdbb94dbc36bd
Div685 <divyeshpatel685@gmail.com>
2021-11-26 22:44:19 +0300

    Add useGoogleSearch hook to searchPage
    - to fetch data from API function

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useStateValue } from '../../StateProvider';
+import useGoogleSearch from '../../useGoogleSearch';
 import './SearchPage.css';
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
+  const { data } = useGoogleSearch(term);
+
+  console.log(data);
+
   return (
     <div className="searchPage">
       <div className="searchPage__header">
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -11,7 +11,7 @@ const useGoogleSearch = (term) => {
     const fetchData = async() => {
       fetch(
         `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
-      ).then(response => response.json)
+      ).then(response => response.json())
       .then(result => {
         setData(result)
       })
