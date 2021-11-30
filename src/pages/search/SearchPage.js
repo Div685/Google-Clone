3a8237440af6081b70ddfea809ffed9fbfefc4ba
Div685 <divyeshpatel685@gmail.com>
2021-11-30 13:49:59 +0300

    Add import to otherIcon options

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -6,6 +6,10 @@ import Response from '../../response';
 import { Link } from 'react-router-dom';
 import Search from './Search';
 import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
