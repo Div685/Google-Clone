15879c1ab48309b38a2bc0267bbd11eb054598a2
Div685 <divyeshpatel685@gmail.com>
2021-11-30 13:55:44 +0300

    Add more icon to searchResultPage

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -10,6 +10,7 @@ import DescriptionIcon from '@material-ui/icons/Description';
 import ImageIcon from '@material-ui/icons/Image';
 import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
