04715eda5433ba5556b11a0f74440d4d421706ed
Div685 <divyeshpatel685@gmail.com>
2021-12-01 13:58:08 +0300

    Add links to the rightside Options
    - to the SearchResult Page

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -62,7 +62,12 @@ function SearchPage() {
           </div>
 
           <div className="searchPage__optionsRight">
-
+          <div className="searchPage__option">
+              <Link to="/settings">Settings</Link>
+            </div>
+            <div className="searchPage__option">
+              <Link to="/tools">Tools</Link>
+            </div>
           </div>
 
         </div>
