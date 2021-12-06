ea85626360b1f7a85358a7a087b80bcbd865a946
Div685 <divyeshpatel685@gmail.com>
2021-12-06 14:58:20 +0300

    fix orverlapping issue on searchPage

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -28,48 +28,48 @@ function SearchPage() {
           <img className="searchPage__logo"
             src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="google_logo"/>
         </Link>
-      </div>
-      <div className="searchPage__headerBody">
-        <Search hideButtons />
+        <div className="searchPage__headerBody">
+          <Search hideButtons />
 
-        <div className="searchPage__options">
+          <div className="searchPage__options">
 
-          <div className="searchPage__optionsLeft">
-            <div className="searchPage__option">
-              <SearchIcon />
-              <Link to="/all">All</Link>
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage__option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage__option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/shopping">shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <RoomIcon />
+                <Link to="/maps">maps</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/more">more</Link>
+              </div>
             </div>
-            <div className="searchPage__option">
-              <DescriptionIcon />
-              <Link to="/news">News</Link>
+
+            <div className="searchPage__optionsRight">
+              <div className="searchPage__option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div className="searchPage__option">
+                <Link to="/tools">Tools</Link>
+              </div>
             </div>
-            <div className="searchPage__option">
-              <ImageIcon />
-              <Link to="/images">Images</Link>
-            </div>
-            <div className="searchPage__option">
-              <LocalOfferIcon />
-              <Link to="/shopping">shopping</Link>
-            </div>
-            <div className="searchPage__option">
-              <RoomIcon />
-              <Link to="/maps">maps</Link>
-            </div>
-            <div className="searchPage__option">
-              <MoreVertIcon />
-              <Link to="/more">more</Link>
-            </div>
+
           </div>
-
-          <div className="searchPage__optionsRight">
-          <div className="searchPage__option">
-              <Link to="/settings">Settings</Link>
-            </div>
-            <div className="searchPage__option">
-              <Link to="/tools">Tools</Link>
-            </div>
-          </div>
-
         </div>
 
       </div>
